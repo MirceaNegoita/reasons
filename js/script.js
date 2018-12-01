ee367bc7be7c333ab959dd27d4bbf911a68708d0
mircea <mircea.negoita13@gmail.com>
2018-12-01 05:57:18 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,99 @@
+function displayFirstReason(){
+    document.getElementById('preview').style.display = 'none';
+    document.getElementById('reason-1').style.display = '';
+}
+
+function displaySecondReason(){
+    document.getElementById('reason-1').style.display = 'none';
+    document.getElementById('reason-2').style.display = '';
+}
+
+function displayThirdReason(){
+    document.getElementById('reason-2').style.display = 'none';
+    document.getElementById('reason-3').style.display = '';
+}
+
+function displayFourthReason(){
+    document.getElementById('reason-3').style.display = 'none';
+    document.getElementById('reason-4').style.display = '';
+}
+
+function displayFifthReason(){
+    document.getElementById('reason-4').style.display = 'none';
+    document.getElementById('reason-5').style.display = '';
+}
+
+function displaySixthReason(){
+    document.getElementById('reason-5').style.display = 'none';
+    document.getElementById('reason-6').style.display = '';
+}
+
+function displaySeventhReason(){
+    document.getElementById('reason-6').style.display = 'none';
+    document.getElementById('reason-7').style.display = '';
+}
+
+function displayEigthReason(){
+    document.getElementById('reason-7').style.display = 'none';
+    document.getElementById('reason-8').style.display = '';
+}
+
+function displayNinthReason(){
+    document.getElementById('reason-8').style.display = 'none';
+    document.getElementById('reason-9').style.display = '';
+}
+
+function displayTenthReason(){
+    document.getElementById('reason-9').style.display = 'none';
+    document.getElementById('reason-10').style.display = '';
+}
+
+function backNinthReason(){
+    document.getElementById('reason-10').style.display = 'none';
+    document.getElementById('reason-9').style.display = '';
+}
+
+function backEigthReason(){
+    document.getElementById('reason-9').style.display = 'none';
+    document.getElementById('reason-8').style.display = '';
+}
+
+function backSeventhReason(){
+    document.getElementById('reason-8').style.display = 'none';
+    document.getElementById('reason-7').style.display = '';
+}
+
+function backSixthReason(){
+    document.getElementById('reason-7').style.display = 'none';
+    document.getElementById('reason-6').style.display = '';
+}
+
+function backFifthReason(){
+    document.getElementById('reason-6').style.display = 'none';
+    document.getElementById('reason-5').style.display = '';
+}
+
+function backFourthReason(){
+    document.getElementById('reason-5').style.display = 'none';
+    document.getElementById('reason-4').style.display = '';
+}
+
+function backThirdReason(){
+    document.getElementById('reason-4').style.display = 'none';
+    document.getElementById('reason-3').style.display = '';
+}
+
+function backSecondReason(){
+    document.getElementById('reason-3').style.display = 'none';
+    document.getElementById('reason-2').style.display = '';
+}
+
+function backFirstReason(){
+    document.getElementById('reason-2').style.display = 'none';
+    document.getElementById('reason-1').style.display = '';
+}
+
+function backPreview(){
+    document.getElementById('reason-1').style.display = 'none';
+    document.getElementById('preview').style.display = '';
+}
